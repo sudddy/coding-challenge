074d4f9a869a95a0068003a7a2e1daaac0cdefb3
sudddy <suddpro@gmail.com>
2020-11-24 18:05:58 +1300

    const for grossProfit

--- constant.js
+++ constant.js
@@ -1,6 +1,8 @@
 const constants = {
   ACCOUNT_REVENUE: "revenue",
-  ACCOUNT_EXPENSE: "expense"
+  ACCOUNT_EXPENSE: "expense",
+  ACCOUNT_TYPE: "sales",
+  VALUE_TYPE: "debit"
 };
 
 module.exports = constants;
