d65cd0a2750fa7869015d8cb632a14d88832bcc1
sudddy <suddpro@gmail.com>
2020-11-24 18:06:17 +1300

    gross profit function

--- index.js
+++ index.js
@@ -9,7 +9,36 @@ var calRevOrExp = (ledger, constant) => {
       return total + data.total_value;
     }, 0);
   console.log(constant, totalValue);
+  return totalValue;
 };
 
-calRevOrExp(ledger, constants.ACCOUNT_REVENUE);
-calRevOrExp(ledger, constants.ACCOUNT_EXPENSE);
+var grossProfit = (ledger, ...rest) => {
+  let data = ledger.data,
+    account_type = rest[0],
+    value_type = rest[1],
+    revenue = rest[2],
+    totalValue = 0;
+
+  let value = data
+    .filter(
+      data =>
+        data.account_type === account_type && data.value_type === value_type
+    )
+    .reduce((total, data) => {
+      return total + data.total_value;
+    }, 0);
+
+  console.log("before", value);
+  totalValue = value / revenue;
+  console.log("value", totalValue);
+  return totalValue;
+};
+
+let revenue = calRevOrExp(ledger, constants.ACCOUNT_REVENUE);
+let expense = calRevOrExp(ledger, constants.ACCOUNT_EXPENSE);
+let grossPro = grossProfit(
+  ledger,
+  constants.ACCOUNT_TYPE,
+  constants.VALUE_TYPE,
+  revenue
+);
