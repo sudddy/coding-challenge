d83ed18b48177eacda6e0e9f5d2cdb606511c532
sudddy <suddpro@gmail.com>
2020-11-24 18:53:51 +1300

    liabilty and assets calculation

--- constant.js
+++ constant.js
@@ -1,8 +1,15 @@
 const constants = {
-  ACCOUNT_REVENUE: "revenue",
-  ACCOUNT_EXPENSE: "expense",
-  ACCOUNT_TYPE: "sales",
-  VALUE_TYPE: "debit"
+  ACCOUNT_CATEGORY_REVENUE: "revenue",
+  ACCOUNT_CATEGORY_EXPENSE: "expense",
+  ACCOUNT_TYPE_SALES: "sales",
+  VALUE_TYPE_DEBIT: "debit",
+  VALUE_TYPE_CREDIT: "credit",
+  ACCOUNT_CATEGORY_ASSETS: "assets",
+  ACCOUNT_CATEGORY_LIABILITY: "liability",
+  ACCOUNT_TYPE_CURRENT: "current",
+  ACCOUNT_TYPE_BANK: "bank",
+  ACCOUNT_TYPE_CURRENT_ACCOUNT_RECEIVABLE: "current_accounts_receivable",
+  ACCOUNT_TYPE_CURRENT_ACCOUNT_PAYABLE: "current_account_payable"
 };
 
 module.exports = constants;
--- index.js
+++ index.js
@@ -12,17 +12,14 @@ var calRevOrExp = (ledger, constant) => {
   return totalValue;
 };
 
-var grossProfit = (ledger, ...rest) => {
+var grossProfit = (ledger, constants, revenue) => {
   let data = ledger.data,
-    account_type = rest[0],
-    value_type = rest[1],
-    revenue = rest[2],
     totalValue = 0;
-
   let value = data
     .filter(
       data =>
-        data.account_type === account_type && data.value_type === value_type
+        data.account_type === constants.ACCOUNT_TYPE_SALES &&
+        data.value_type === constants.VALUE_TYPE_DEBIT
     )
     .reduce((total, data) => {
       return total + data.total_value;
@@ -34,11 +31,91 @@ var grossProfit = (ledger, ...rest) => {
   return totalValue;
 };
 
-let revenue = calRevOrExp(ledger, constants.ACCOUNT_REVENUE);
-let expense = calRevOrExp(ledger, constants.ACCOUNT_EXPENSE);
-let grossPro = grossProfit(
-  ledger,
-  constants.ACCOUNT_TYPE,
-  constants.VALUE_TYPE,
-  revenue
-);
+var netProfit = (expense, revenue) => {
+  let remainder = revenue - expense;
+  let value = remainder / revenue;
+  console.log("value", value);
+  return value;
+};
+
+var calculateAssets = (ledger, constants) => {
+  let data = ledger.data,
+    totalValue = 0;
+
+  let addValues = data
+    .filter(
+      data =>
+        data.account_category === constants.ACCOUNT_CATEGORY_ASSETS &&
+        data.value_type === constants.VALUE_TYPE_DEBIT &&
+        (data.account_type === constants.ACCOUNT_TYPE_CURRENT ||
+          data.account_type === constants.ACCOUNT_TYPE_BANK ||
+          data.account_type ===
+            constants.ACCOUNT_TYPE_CURRENT_ACCOUNT_RECEIVABLE)
+    )
+    .reduce((total, data) => {
+      return total + data.total_value;
+    }, 0);
+
+  let subractvalues = data
+    .filter(
+      data =>
+        data.account_category === constants.ACCOUNT_CATEGORY_ASSETS &&
+        data.value_type === constants.VALUE_TYPE_CREDIT &&
+        (data.account_type === constants.ACCOUNT_TYPE_CURRENT ||
+          data.account_type === constants.ACCOUNT_TYPE_BANK ||
+          data.account_type ===
+            constants.ACCOUNT_TYPE_CURRENT_ACCOUNT_RECEIVABLE)
+    )
+    .reduce((total, data) => {
+      return total - data.total_value;
+    }, 0);
+
+  totalValue = addValues + subractvalues;
+
+  console.log("assets", totalValue);
+  console.log("addValues", addValues);
+  console.log("subractValue", subractvalues);
+  return totalValue;
+};
+
+var calculateLiabilites = (ledger, constants) => {
+  let data = ledger.data,
+    totalValue = 0;
+
+  let addValues = data
+    .filter(
+      data =>
+        data.account_category === constants.ACCOUNT_CATEGORY_LIABILITY &&
+        data.value_type === constants.VALUE_TYPE_CREDIT &&
+        (data.account_type === constants.ACCOUNT_TYPE_CURRENT ||
+          data.account_type === constants.ACCOUNT_TYPE_CURRENT_ACCOUNT_PAYABLE)
+    )
+    .reduce((total, data) => {
+      return total + data.total_value;
+    }, 0);
+
+  let subractvalues = data
+    .filter(
+      data =>
+        data.account_category === constants.ACCOUNT_CATEGORY_ASSETS &&
+        data.value_type === constants.VALUE_TYPE_CREDIT &&
+        (data.account_type === constants.ACCOUNT_TYPE_CURRENT ||
+          data.account_type === constants.ACCOUNT_TYPE_CURRENT_ACCOUNT_PAYABLE)
+    )
+    .reduce((total, data) => {
+      return total - data.total_value;
+    }, 0);
+
+  totalValue = addValues + subractvalues;
+  console.log("liability", totalValue);
+  console.log("addValues", addValues);
+  console.log("subractValue", subractvalues);
+  return totalValue;
+};
+
+var revenue = calRevOrExp(ledger, constants.ACCOUNT_CATEGORY_REVENUE);
+var expense = calRevOrExp(ledger, constants.ACCOUNT_CATEGORY_EXPENSE);
+var grossPro = grossProfit(ledger, constants, revenue);
+var netPro = netProfit(expense, revenue);
+var calAssets = calculateAssets(ledger, constants);
+var calLiab = calculateLiabilites(ledger, constants);
